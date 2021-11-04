ff6f974f300d3d55965d2216c5a8b1e57cda013a
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-11-04 17:22:09 +0300

    bot finish

--- bot.js
+++ bot.js
@@ -1,14 +1,22 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 const api = require('covid19-api')
+const COUNTRY_LIST = require('./constant')
+
 
 const bot = new Telegraf('2094406632:AAFYLt5fU61fLtST2ztwmgSsWusEA4aS48A')
-bot.start((ctx) => ctx.reply(`Salam alekum ${ctx.message.from.first_name}`))
-
-
-
+bot.start((ctx) => ctx.reply(`
+Добрый день ${ctx.message.from.first_name}!
+Узнай статистику по коронавирусу.
+Введи на английском название страны и получи статистику.
+Посмотреть весь список стран можно командой /help.`, 
+Markup.keyboard([
+  ['US', 'Russia'],
+  ['Azerbaijan', 'Kazakhstan']
+])
+))
+bot.help((ctx) => ctx.reply(COUNTRY_LIST))
 bot.on('text',async(ctx) => {
 let data = {}
-
 try{
 data = await api.getReportsByCountries(ctx.message.text)
 const formData = `
@@ -16,17 +24,13 @@ const formData = `
 Случаи: ${data[0][0].cases}
 Смертей: ${data[0][0].deaths}
 Вылечились: ${data[0][0].recovered}
- `
- ctx.reply(formData) 
- 
-
+ ` 
+ ctx.reply(formData)
 } catch { 
-  ctx.reply('Ошибка такой страны не существует')
-}
-
-  
+  ctx.reply('Ошибка такой страны не существует, посмотрите /help')
+} 
 })
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
 bot.launch()
 
 console.log('бот запущен')
